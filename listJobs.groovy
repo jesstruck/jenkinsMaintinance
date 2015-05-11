bfc51e1796f1f6cf7c3c70b41fa2b5c1c8d20b59
Jes Struck <jes@praqma.net>
2015-05-11 16:35:32 +0200

    Update listJobs.groovy

--- listJobs.groovy
+++ listJobs.groovy
@@ -8,15 +8,18 @@ def updated = 0
 def counter = 0
 jobs = Hudson.instance.items
 for (item in jobs){
-  if(item.name.startsWith('SOMETHING-')){
+  if(item.name.startsWith('SOMETHING')){
     counter++
   	for (publisher in item.publishersList){
     	if(publisher instanceof Mailer) {
-    		println(item.name + " " + publisher.recipients)// + " " + publisher.properties)
-          	updated++
+          if (publisher.sendToIndividuals == false){
+    			println(item.name +' '+ publisher.recipients +' '+ publisher.sendToIndividuals)
+          		updated++
+          }
         }
   	}
   }
 }
 
-println(counter-updated)
+println("Total job: "+counter)
+println("Misconfigured jobs" + updated)
