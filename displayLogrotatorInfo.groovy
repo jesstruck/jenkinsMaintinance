f9d7de443bd5a84094121aff617291f06df3b10d
Jes Struck <mail@jesstruck.dk>
2017-02-07 16:03:32 +0100

    Update displayLogrotatorInfo.groovy

--- displayLogrotatorInfo.groovy
+++ displayLogrotatorInfo.groovy
@@ -1,6 +1,6 @@
 /**
 This Script loops trough all your Jenkins jobs, and reports all the jobs where the configuration of the logRotator is out of standard
-This print out each Job in the format ${jobName} -> {daysToKeep}
+This print out each Job in the format ${jobName} DaysToKeep {daysToKeep} #builds ${#number of builds for the job}
 and in the buttom a total count of misconfigured jobs.
 
 Parameters:
@@ -18,7 +18,7 @@ jenkins.model.Jenkins.instance.getAllItems(hudson.model.Job).each {
       	(it.logRotator.daysToKeep<0 || it.logRotator.daysToKeep> daysToKeep) &&
       	(it.logRotator.artifactDaysToKeep < 0 ||it.logRotator.artifactDaysToKeep < aDaysToKeep )
        ){
-      	println ""+it.fullDisplayName + " -> "+ it.logRotator.daysToKeep	
+      	println ""+it.fullDisplayName + " DaysToKeep ->  "+ it.logRotator.daysToKeep + " #builds " + it.builds.size()
       	counter++
 //        it.logRotator = new hudson.tasks.LogRotator ( 14, -1, 14, -1) // days to keep, num to keep, artifact days to keep, num to keep
     }
