dc4d7b53d2c5e7e515420b4630269df0656c7618
Jes Struck <mail@jesstruck.dk>
2017-02-07 16:00:43 +0100

    Update displayLogrotatorInfo.groovy

--- displayLogrotatorInfo.groovy
+++ displayLogrotatorInfo.groovy
@@ -5,20 +5,22 @@ and in the buttom a total count of misconfigured jobs.
 
 Parameters:
 _________________
-* daysToKeep -> The number of days that you wish to keep jobs
-
+* daysToKeep  -> The number of days that you wish to keep logs and build info
+* adaysToKeep -> The number of days to keep artifacts
 */
-daysToKeep = 14
-
+daysToKeep  = 14 
+aDaysToKeep = 14
 
 counter = 0
 jenkins.model.Jenkins.instance.getAllItems(hudson.model.Job).each {
   if(it.logRotator!=null){
-    if (it.logRotator.daysToKeep<0 || it.logRotator.daysToKeep> daysToKeep){
+    if (
+      	(it.logRotator.daysToKeep<0 || it.logRotator.daysToKeep> daysToKeep) &&
+      	(it.logRotator.artifactDaysToKeep < 0 ||it.logRotator.artifactDaysToKeep < aDaysToKeep )
+       ){
+      	println ""+it.fullDisplayName + " -> "+ it.logRotator.daysToKeep	
       	counter++
-        //potentially you can add a new logrotator here
-        //job.logRotator = new hudson.tasks.LogRotator ( 30, 40, 30, 10) // days to keep, num to keep, artifact days to keep, num to keep
-  		  println ""+it.fullDisplayName + " -> "+ it.logRotator.daysToKeep
+//        it.logRotator = new hudson.tasks.LogRotator ( 14, -1, 14, -1) // days to keep, num to keep, artifact days to keep, num to keep
     }
   }
 } 
